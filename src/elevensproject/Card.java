4e90cb31a3a488a4a5902fdec1ddac013f4fc54d
Tyler Tran <trant2070@my.sduhsd.net>
2019-03-04 09:03:22 -0800

    new project

---
+++ src/elevensproject/Card.java
@@ -0,0 +1,49 @@
+package elevensproject;
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+public class Card
+{
+	public static final String FACES[] = {"ZERO","ACE","TWO","THREE","FOUR",
+			"FIVE","SIX","SEVEN","EIGHT","NINE","TEN","JACK","QUEEN","KING"};
+
+	//instance variables
+		String suit;
+		int face;
+
+  	//constructors
+        public Card(String s, int f)
+        {
+            suit = s;
+            face = f;
+        }
+        
+
+	// modifiers
+		//set methods
+        public void setSuit(String s)
+        {
+            suit = s;
+        }
+        
+        public void setFace(int f)
+        {
+            face = f;
+        }
+  	//accessors
+		//get methods
+        public String getSuit()
+        {
+            return suit;
+        }
+        
+        public int getFace()
+        {
+            return face;
+        }
+
+  	//toString
+
+ }
